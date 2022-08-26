d29a93c4f2ebf5047c4a7795efd63bb257f1aeb1
foxstudent101846 <1jaO8q1uTHC7>
2022-08-26 11:59:44 +0300

    add empty project

---
+++ Artsofte/Artsofte/Artsofte/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Artsofte.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
